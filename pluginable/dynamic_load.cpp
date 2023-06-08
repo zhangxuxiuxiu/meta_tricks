9cc06b36d545c1ae4a80a67827751df06888ec3e
Abner Zhang <abner.zhang@shopee.com>
2023-06-08 17:10:51 +0800

    add plugin_three in binary

--- pluginable/dynamic_load.cpp
+++ pluginable/dynamic_load.cpp
@@ -4,11 +4,27 @@
 #include <iostream>
 #include <string>
 
+#include <boost/dll/alias.hpp> // for BOOST_DLL_ALIAS_SECTIONED
+
+namespace dynamic{
+
+	class PluginThree: public Pluginable{
+		public:
+			std::string Load() override {
+				return __PRETTY_FUNCTION__;
+			}
+	};
+
+	PluginThree plugin_three;
+	BOOST_DLL_ALIAS_SECTIONED(dynamic::plugin_three, plugin_three, PLUGIN_SECTION_NAME)
+}
+
+
 int main(int argc,char* argv[]){
-	std::vector<std::string> dylibs(argc-1);
-	for(auto i=1; i< argc; ++i){
-		dylibs[i-1] = argv[i];
-		std::cout << "libname: " << dylibs[i-1] << '\n';
+	std::vector<std::string> dylibs(argc);
+	for(auto i=0; i< argc; ++i){
+		dylibs[i] = argv[i];
+		std::cout << "libname: " << dylibs[i] << '\n';
 	}
 
 	dynamic::LoadAllPlugin(dylibs);
