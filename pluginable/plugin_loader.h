79d8badd3b15f9ba9233d8c33629b77b06a4f942
Abner Zhang <abner.zhang@shopee.com>
2023-06-12 10:24:19 +0800

    rename exports to symbols

--- pluginable/plugin_loader.h
+++ pluginable/plugin_loader.h
@@ -14,15 +14,15 @@ namespace dynamic{
 			// Class `library_info` can extract information from a library
 			boost::dll::library_info inf(dylibName);
 
-			std::vector<std::string> exports = inf.symbols( sectName );
-			if( exports.empty() ){
+			std::vector<std::string> symbols = inf.symbols( sectName );
+			if( symbols.empty() ){
 				continue;
 			}
 			// Loading library and importing symbols from it
 			auto lib = boost::make_shared<boost::dll::shared_library>( dylibName );
-			for (std::size_t j = 0; j < exports.size(); ++j) {
+			for (auto& sym :  symbols ) {
 				// each plugin adds one reference cnt to $lib to keep $lib from being unloaded so that the plugin is valid
-				plugins.push_back( boost::shared_ptr<PluginApi>( lib, lib->get<PluginApi*>( exports[j] )) );
+				plugins.push_back( boost::shared_ptr<PluginApi>( lib, lib->get<PluginApi*>( sym )) );
 			}
 		}
 
