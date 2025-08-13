50d3c87b563f9e1b834dd49d81b554187a8db049
ravinezhang <ravinezhang@tencent.com>
2025-08-13 20:37:33 +0800

    support version pick through overload

--- overload.cpp
+++ overload.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <type_traits>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
 
+using namespace std;
 
 template<size_t N>
 struct OL : OL<N-1>{};
@@ -8,6 +13,9 @@ struct OL : OL<N-1>{};
 template<>
 struct OL<0>{};
 
+/*
+    for all $impl that meets enable_if, the highest OL verison is choosen  
+*/
 template<class T>
 struct Dispatcher{
 
@@ -18,23 +26,23 @@ struct Dispatcher{
 	}
 	
 	template<class U>
-	static constexpr size_t impl(typename std::enable_if< std::is_integral<U>::value, OL<2>>::type ){
+	static constexpr size_t impl(typename enable_if< is_integral<U>::value, OL<2>>::type ){
 		return 4;
 	}
 	
 	template<class U>
-	static constexpr size_t impl(typename std::enable_if< std::is_same<U, float>::value, OL<4>>::type ){
+	static constexpr size_t impl(typename enable_if< is_same<U, float>::value, OL<4>>::type ){
 		return 3;
 	}
 
 	template<class U>
-	static constexpr size_t impl(typename std::enable_if< std::is_floating_point<U>::value, OL<6>>::type ){
+	static constexpr size_t impl(typename enable_if< is_floating_point<U>::value, OL<6>>::type ){
 		return 2;
 	}
 
 	// highest priority
 	template<class U>
-	static constexpr size_t impl(typename std::enable_if< std::is_same<U, int>::value, OL<8>>::type ){
+	static constexpr size_t impl(typename enable_if< is_same<U, int>::value, OL<8>>::type ){
 		return 1;
 	}
 
@@ -43,6 +51,27 @@ struct Dispatcher{
 		return impl<T>(OL<N>{});
 	}
 };
+
+
+/*
+invoke the highest OL version	
+*/
+template<class U, class KeyType>
+static auto Find(U const & container, KeyType const& key, OL<1>) -> decltype(container.find(key)) {
+	cout << "container.find by " << key << '\n';
+	return container.find(key);
+}
+
+template<class U, class KeyType>
+static auto Find(U const& container, KeyType const& key, OL<0>) -> decltype( find(begin(container), end(container), key) ) {
+	cout << "std::find by " << key << '\n';
+	return find(begin(container), end(container), key);
+}
+
+template<class Container,class KeyType>
+static auto Find(Container const& container, KeyType const& key){
+	return Find(container, key, OL<10>{});
+}
 
 int main(){
 	static_assert( Dispatcher<int>::value<0>() == 5);
@@ -85,4 +114,25 @@ int main(){
 	static_assert( Dispatcher<double>::value<7>() == 2);
 	static_assert( Dispatcher<double>::value<8>() == 2);
 
+	//test cases for  Finder 
+	// case 1: no find in vector
+	vector<int> vec1 = {1,2,3};	
+	auto it = Find(vec1, 3);  // ==> std::find
+	cout << "vector got 3: " << *it << endl;
+	auto it1 = Find(vec1, 3, OL<1>{}); // ==> std::find
+	cout << "vector got 3: " << *it1 << endl;
+
+	// case 2: find in map, std::find not support
+	map<int,int> m2 = {{1,-1}, {2,-2}};
+	auto it2 = Find(m2, 2); // ==> map::find
+	cout << "map got -2: " << it2->second << endl;
+
+	// case 3: find in set, std::find support too
+	set<int> set1 = {1,2,3};	
+	auto it3 = Find(set1, 3); // ==> set::find
+	cout << "set got 3: " << *it3 << endl;
+	auto it4 = Find(set1, 3, OL<0>{}); // ==> std::find
+	cout << "set got 3: " << *it4 << endl;
+
+	return 0;
 }
